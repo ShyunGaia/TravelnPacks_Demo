3e5ec8c870b5a6be41a2e26461ddd40757f1ac27
ShyunGaia <sh.yun@gaiagroup.co.kr>
2023-11-22 14:02:59 +0900

    revoke rootPath

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../../', 'static'),
+      rootPath: join(__dirname, '..', 'static'),
   }),],
   controllers: [AppController],
   providers: [AppService],
